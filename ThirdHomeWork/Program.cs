06864888a8c5f30523ca03a3ac4b33dbbf9e1803
Нурбек Джабашев <invokedex@gmail.com>
2019-01-14 00:08:55 +0600

    Добавьте файлы проекта.

---
+++ ThirdHomeWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdHomeWork
+{
+    //1.Написать программу, которая считывает символы с клавиатуры, пока не будет введена точка. Программа должна сосчитать количество введенных пользователем пробелов.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countSpaces = 0;
+            char Space = ' ';
+
+            while (Space != '.')
+            {
+                Space = (char)Console.Read();
+                if (Space == ' ')
+                    countSpaces++;
+            }
+
+            Console.WriteLine("Количество введенных пробелов: " + countSpaces);
+        }
+    }
+}
